6d041cb13230003891fae12852b1f723a5d75ae2
Turkel Gadirzade <gadirzade@gmail.com>
2022-03-17 16:02:21 +0400

    vault tested

---
+++ certs/vault.bash
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+PROFILE=${PROFILE:-dev}
+XPATH=paas2/k8s/${PROFILE}
+VAULES_FILE=${VAULES_FILE:-values-injector.yaml}
+NAMESPACE=${NAMESPACE:-vault}
+
+errorExit () {
+    echo -e "\nERROR: $1"; echo
+    exit 1
+}
+
+usage () {
+    cat << END_USAGE
+ <options>
+--profile           : [required] profile
+--ns                : [optional] namespace
+--f                 : [optinal] values file to override defaults. Default is values-injector.yaml
+END_USAGE
+
+    exit 1
+}
+
+processOptions () {
+    while [[ $# > 0 ]]; do
+        case "$1" in      
+            --profile)
+                PROFILE=${2}; shift 2
+            ;;  
+            --ns)
+                NAMESPACE=${2}; shift 2
+            ;;  
+            --f)
+                VAULES_FILE=${2}; shift 2
+            ;;                                                                                              
+            -h | --help)
+                usage
+            ;;
+            *)
+                usage
+            ;;
+        esac
+    done
+}
+
+main () {
+    echo -e "\nRunning"
+
+    echo "PROFILE:  ${PROFILE}"  
+    echo "VAULES_FILE:  ${VAULES_FILE}" 
+    echo "NAMESPACE:  ${NAMESPACE}"   
+    echo "PATH:  ${XPATH}"   
+
+    # getReviewerJWTToken
+    # getKubeCertAndHost
+    # enableKubeAuth
+    # configureKubeAuth
+    # createPolicy
+    # createRoleAndAttachPolicy
+    createCertificates
+    saveCertificatesToVault
+}
+
+installVault() {
+    git clone https://github.com/paas2/vault 
+    helm upgrade vault ./vault/helm-charts/vault -f ./vault/helm-charts/vault/values.yaml -f ./vault/helm-charts/vault/${VAULES_FILE} --namespace vault --create-namespace --install
+}
+
+getReviewerJWTToken() {
+    export VAULT_SA_NAME=$(kubectl -n ${NAMESPACE} get sa vault-agent-injector --output jsonpath="{.secrets[*]['name']}")
+    export SA_JWT_TOKEN=$(kubectl -n ${NAMESPACE} get secret $VAULT_SA_NAME --output 'go-template={{ .data.token }}' | base64 --decode)
+    echo $SA_JWT_TOKEN > ~/cluster-jwt-token
+}
+
+getKubeCertAndHost(){
+    KUBE_CA_CERT=$(kubectl config view --raw --minify --flatten --output='jsonpath={.clusters[].cluster.certificate-authority-data}' | base64 --decode)
+    echo $KUBE_CA_CERT > ~/cluster-ca.crt   
+    KUBE_HOST=$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " ") 
+    echo $KUBE_HOST > ~/cluster-kube-host     
+}
+
+enableKubeAuth() {
+    vault login
+    vault auth enable --path=${XPATH} kubernetes
+}
+
+configureKubeAuth(){
+    vault write auth/${XPATH}/config \
+    token_reviewer_jwt="$(cat ~/cluster-jwt-token)" \
+    kubernetes_host="$(cat ~/cluster-kube-host)" \
+    kubernetes_ca_cert="$(cat ~/cluster-ca.crt)" \
+    issuer="https://kubernetes.default.svc.cluster.local"      
+}
+
+createPolicy() {
+    vault policy write ${PROFILE} - <<EOF
+    path "${XPATH}/certs/*" {
+        capabilities = ["read"]
+    }
+EOF
+}
+
+createRoleAndAttachPolicy() {
+    vault write auth/${XPATH}/role/${PROFILE} \
+        bound_service_account_names=default \
+        bound_service_account_namespaces=default \
+        policies=dev-app \
+        ttl=24h    
+
+    # vault write auth/dev-cluster/role/devweb-app \
+    #     bound_service_account_names=* \
+    #     bound_service_account_namespaces=* \
+    #     policies=dev-app \
+    #     ttl=24h
+}
+
+createCertificates() {
+    sh generate-certificates.sh
+}
+
+saveCertificatesToVault() {
+    vault secrets enable -path=${XPATH} kv-v2
+    vault kv put ${XPATH}/certs/ca-cert @ca-cert.pem 
+    vault kv put ${XPATH}/certs/ca-key @ca-key.pem  
+    vault kv put ${XPATH}/certs/cert-chain @cert-chain.pem    
+    vault kv put ${XPATH}/certs/root-cert @root-cert.pem  
+}
+
+
+processOptions $*
+main
+
--- k8s/prov.bash
+++ k8s/prov.bash
@@ -107,39 +107,6 @@ main () {
     installArgoCd 
 }
 
-configureVault() {
-    PATH=paas2/k8s/${PROFILE}
-
-    vault login
-    vault auth enable --path=${PATH} kubernetes
-
-    # vault write auth/${PROFILE}/config \
-    # token_reviewer_jwt="$(cat ~/dev-cluster-jwt-token)" \
-    # kubernetes_host=https://192.168.20.22 \
-    # kubernetes_ca_cert="$(cat ~/dev-ca.crt)" \
-    # issuer="https://kubernetes.default.svc.cluster.local"
-
-    vault secrets enable -path=${PATH} kv-v2
-    vault kv put ${PATH}/certs/ca-cert @ca-cert.pem 
-    vault kv put ${PATH}/certs/ca-key @ca-key.pem  
-    vault kv put ${PATH}/certs/cert-chain @cert-chain.pem    
-    vault kv put ${PATH}/certs/root-cert @root-cert.pem     
-
-    vault policy write ${PROFILE} - <<EOF
-    path “${PATH}/certs/*” {
-        capabilities = ["read"]
-    }
-EOF
-
-    vault write auth/dev-cluster/role/devweb-app \
-        bound_service_account_names=default \
-        bound_service_account_namespaces=default \
-        policies=dev-app \
-        ttl=24h
-
-}
-
-
 processOptions $*
 main
 
